e96e820e0e0fad64794e1c0414489243677625bd
Adam McKellar <dev@mckellar.eu>
2024-09-14 14:29:10 +0200

    feat: package struct and display trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,66 @@
 //          https://www.boost.org/LICENSE_1_0.txt)
 
 
+use std::env;
+use std::fmt;
+
+use bincode::{config, Decode, Encode};
+use lz4_flex::block::{compress_prepend_size, decompress_size_prepended};
+
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq)]
+pub struct Package {
+    name: String,
+    version: String,
+    authors: Option<Vec<String>>,
+    description: Option<String>,
+    homepage: Option<String>,
+    repository: Option<String>,
+    license_identifier: Option<String>,
+    license_text: Option<String>,
+}
+
+#[derive(Encode, Decode, Debug, PartialEq, Eq)]
+pub struct PackageList(Vec<Package>);
+
+
+impl fmt::Display for PackageList {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        const SEPERATOR_WIDTH: usize = 80;
+        let separator: String = "=".repeat(SEPERATOR_WIDTH);
+
+        writeln!(f, "{}\n", separator)?;
+
+        for package in self.0.iter() {
+            writeln!(f, "Package:     {} {}", package.name, package.version)?;
+            if let Some(description) = &package.description {
+                writeln!(f, "Description: {}", description)?;
+            }
+            if let Some(authors) = &package.authors {
+                writeln!(f, "Authors:")?;
+                for author in authors.iter() {
+                    writeln!(f, " - {}", author)?;
+                }
+            }
+            if let Some(homepage) = &package.homepage {
+                writeln!(f, "Homepage:    {}", homepage)?;
+            }
+            if let Some(repository) = &package.repository {
+                writeln!(f, "Repository:  {}", repository)?;
+            }
+            if let Some(license_identifier) = &package.license_identifier {
+                writeln!(f, "SPDX Ident.: {}", license_identifier)?;
+            }
+
+            writeln!(f, "\n{}\n", separator)?;
+            
+            if let Some(license_text) = &package.license_text {
+                writeln!(f, "{}\n{}\n", license_text, separator)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+
