7db078fad7a551b51e24fc14394bcb65100de1c8
Adam McKellar <dev@mckellar.eu>
2025-01-27 19:31:34 +0100

    fix: using only one registry src folder for fetching licenses

--- src/build_script/cargo_source.rs
+++ src/build_script/cargo_source.rs
@@ -3,18 +3,16 @@
 //         (See accompanying file LICENSE or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
 
-
-use std::path::PathBuf;
 use std::env::var_os;
 use std::fs::{read_dir, read_to_string};
+use std::path::PathBuf;
 
 use directories::BaseDirs;
-use log::{info, warn, trace};
+use log::{info, trace, warn};
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 use crate::PackageList;
-
 
 fn cargo_folder() -> PathBuf {
     if let Some(path) = var_os("CARGO_HOME") {
@@ -26,27 +24,24 @@ fn cargo_folder() -> PathBuf {
     }
 }
 
-fn src_folder(path: PathBuf) -> PathBuf {
+fn src_registry_folders(path: PathBuf) -> Vec<PathBuf> {
     let src_subfolder = PathBuf::from("registry/src");
     let src_dir = path.join(src_subfolder);
-    read_dir(src_dir).expect("Src path is not a dir.").filter(|p| {
-        if let Ok(entry) = p {
-            return entry.path().is_dir();
-        }
-        false
-    }).next().expect("Failed to find src directory in .cargo/registry/src")
-    .expect("Failed unwrapping src dir.")
-    .path()
+    read_dir(src_dir)
+        .expect("Src path is not a dir.")
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().map_or(false, |ft| ft.is_dir()))
+        .map(|e| e.path())
+        .collect()
 }
 
-pub(super) fn license_text_from_folder(path: &PathBuf) ->  Option<String> {
+pub(super) fn license_text_from_folder(path: &PathBuf) -> Option<String> {
     trace!("Fetching license in folder: {:?}", &path);
 
     let entries = read_dir(&path).unwrap();
 
-    static LICENSE_FILE_NAME_REGEX: Lazy<Regex> = Lazy::new(|| {
-        Regex::new(r"(?i).*(license|copying|authors|notice|eula).*").unwrap()
-    });
+    static LICENSE_FILE_NAME_REGEX: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"(?i).*(license|copying|authors|notice|eula).*").unwrap());
 
     let mut potential_license_files = vec![];
 
@@ -80,23 +75,24 @@ pub(super) fn license_text_from_folder(path: &PathBuf) ->  Option<String> {
 }
 
 pub(super) fn licenses_text_from_cargo_src_folder(package_list: &mut PackageList) {
-    let src_folder = src_folder(cargo_folder());
-    info!("src folder: {:?}", &src_folder);
+    for src_folder in src_registry_folders(cargo_folder()) {
+        info!("src folder: {:?}", &src_folder);
 
-    for folder in read_dir(src_folder)
-                            .expect("Failed reading source folder.")
-                            .into_iter()
-                            .filter(|e| e.is_ok())
-                            .map(|e| e.unwrap())
-                            .filter(|e| e.path().is_dir())
-                            .map(|e| e.path()) {
-        let folder_name = folder.as_path().iter().last().unwrap().to_str().unwrap();
-        for package in package_list.iter_mut()
-                                    .filter(|p| p.license_text.is_none()) {
-            if folder_name.starts_with(&package.name) && folder_name.ends_with(&package.version) {
-                info!("Fetching license for: {}", &package.name);
-                package.license_text = license_text_from_folder(&folder);
+        for folder in read_dir(src_folder)
+            .expect("Failed reading source folder.")
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.path().is_dir())
+            .map(|e| e.path())
+        {
+            let folder_name = folder.as_path().iter().last().unwrap().to_str().unwrap();
+            for package in package_list.iter_mut().filter(|p| p.license_text.is_none()) {
+                if folder_name.starts_with(&package.name) && folder_name.ends_with(&package.version)
+                {
+                    info!("Fetching license for: {}", &package.name);
+                    package.license_text = license_text_from_folder(&folder);
+                }
             }
         }
     }
-} 
+}
