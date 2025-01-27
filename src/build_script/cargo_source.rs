735d0bf2ddb5bd98744b23b0dc8ab61aa2b41290
Adam McKellar <dev@mckellar.eu>
2025-01-27 22:13:23 +0100

    chore: update license years

--- src/build_script/cargo_source.rs
+++ src/build_script/cargo_source.rs
@@ -1,4 +1,4 @@
-//               Copyright Adam McKellar 2024
+//               Copyright Adam McKellar 2024, 2025
 // Distributed under the Boost Software License, Version 1.0.
 //         (See accompanying file LICENSE or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
