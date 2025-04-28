e07a818e34221f5ecbcb79a92644e0af52a1a394
Adam McKellar <dev@mckellar.eu>
2025-04-28 18:24:36 +0200

    tests: added color detection and enable color for error print

--- src/build/debug.rs
+++ src/build/debug.rs
@@ -1,20 +1,27 @@
 use std::sync::Once;
 
-static LOGGER_ONCE: Once = Once::new();
+use error_stack::Report;
+
+static SETUP_ONCE: Once = Once::new();
 
 pub(crate) fn setup_logger() {
     use simplelog::Config;
 
-    LOGGER_ONCE.call_once(|| {
-        simplelog::TermLogger::new(
-            log::LevelFilter::Debug,
-            Config::default(),
-            simplelog::TerminalMode::Mixed,
-            simplelog::ColorChoice::Auto,
-        );
-    });
+    simplelog::TermLogger::new(
+        log::LevelFilter::Debug,
+        Config::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Auto,
+    );
 }
 
 pub(crate) fn setup_test() {
-    setup_logger();
+    SETUP_ONCE.call_once(|| {
+        setup_logger();
+        if supports_color::on_cached(supports_color::Stream::Stderr)
+            .is_some_and(|level| level.has_basic)
+        {
+            Report::set_color_mode(error_stack::fmt::ColorMode::Color);
+        }
+    });
 }
