a35c0a1018ea7093f86d61583316a82d8d591e6c
Adam McKellar <dev@mckellar.eu>
2025-04-28 21:26:44 +0200

    refactor: changed CPath to hold PathBuf

--- src/build/error.rs
+++ src/build/error.rs
@@ -1,22 +1,22 @@
-use std::{ffi::OsStr, fmt};
+use std::{ffi::OsStr, fmt, path::PathBuf};
 
 #[derive(Debug, Clone)]
-pub struct CPath(String);
+pub struct CPath(pub PathBuf);
 
 impl<T: AsRef<OsStr>> From<T> for CPath {
     fn from(value: T) -> Self {
-        Self(value.as_ref().to_string_lossy().to_string())
+        Self(value.as_ref().into())
     }
 }
 
 impl fmt::Display for CPath {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Path: {}", self.0)
+        write!(f, "Path: {}", self.0.to_string_lossy())
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct CEnvVar(String);
+pub struct CEnvVar(pub String);
 
 impl<T: AsRef<OsStr>> From<T> for CEnvVar {
     fn from(value: T) -> Self {
