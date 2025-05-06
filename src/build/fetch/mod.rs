b27b29b7885307b8ddfe8a366e9555faa0428929
Adam McKellar <dev@mckellar.eu>
2025-05-06 11:56:38 +0200

    docs: made function that is public for benches hidden in docs

--- src/build/fetch/mod.rs
+++ src/build/fetch/mod.rs
@@ -91,6 +91,7 @@ pub(crate) fn license_text_from_folder(path: &PathBuf) -> Result<Option<String>,
 ///
 /// If a package was loaded from a cache, it is ignored.
 /// Failure of reading directories of packages are ignored.
+#[doc(hidden)]
 pub fn populate_package_list_licenses(
     package_list: &mut PackageList,
     cargo_home_dir: PathBuf,
