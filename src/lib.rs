3e2f96689e9c7ddcab90a27c8ed75711259e80d5
Adam McKellar <dev@mckellar.eu>
2026-04-28 20:08:25 +0200

    chore: update copyright notice year

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright Adam McKellar 2024, 2025
+// Copyright Adam McKellar 2024, 2025, 2026
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
