619fd53fbc7e8f7f35e0c411582b4cf2d6e53305
Adam McKellar <dev@mckellar.eu>
2026-05-29 09:31:38 +0200

    feat: add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,24 @@
+// Copyright Adam McKellar 2026
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/*!
+ * The prelude of `license-fetcher`.
+ *
+ * ```
+ * use license_fetcher::prelude::*;
+ * ```
+ */
+
+pub use crate::{read_package_list_from_out_dir, Package, PackageList};
+
+#[cfg(feature = "build")]
+pub use crate::build::{
+    fetcher::{
+        error::{LicenseFetcherError, EK},
+        package_list, package_list_with_licenses,
+    },
+    write::error::WriteError,
+};
